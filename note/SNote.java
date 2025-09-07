d5038c029f47df0804f0b24e2679a4e291d6119a
tpebop <tpebop54@gmail.com>
2025-09-07 16:53:17 -0600

    structure

---
+++ note/SNote.java
@@ -0,0 +1,10 @@
+package note;
+
+public class SNote {
+    public void text() {}
+    public void x2() {}
+    public void play() {}
+    public void rp(int n) {}
+    public void s2() {}
+    public void lp(int n) {}
+}
